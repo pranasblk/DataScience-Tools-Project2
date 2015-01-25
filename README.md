b78df986ddecb991f5e098eed901f7aa590f0402
Pranas Baliuka <pranasblk@gmail.com>
2015-01-25 12:30:21 +1100

    Add doc, server and ui files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# DataScience-Tools-Project2
-coursera.com / Data Science / Developing Data Products / Project 2
+# Project
+The project is related to  coursera.com 
+ * Specialization: Data Science
+ * Course: Developing Data Products 
+ * Project1 / Part 2
+ 
+# How to run
+ * Install devtools `install.packages(“devtools”)`
+ * Install shiny `install.packages("shiny")`
+ * Run the application `library(shiny);runGitHub('DataScience-Tools-Project2', 'pranasblk')`
---
+++ ui.R
@@ -0,0 +1,38 @@
+library(shiny)
+library(ggplot2)
+
+## Adaptation of sample shiny applicatio to explore `mtcars` dataset.
+## Inspired by the project of 
+## Regression Models to predict `mpg` and see if `am` has impact on `mpg`.
+dataset <- mtcars
+
+shinyUI(pageWithSidebar(
+  
+  headerPanel("Cars Explorer"),
+  
+  sidebarPanel(
+    # The number of cars to display
+    sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
+                value=min(32, nrow(dataset)), step=1, round=0),
+    
+    # The X axis
+    selectInput('x', 'X', names(dataset), "hp"),
+    
+    # The Y axis 
+    selectInput('y', 'Y', names(dataset), "mpg"),
+    
+    # Colour of data points
+    selectInput('color', 'Color', c('None', names(dataset)), "wt"),
+    
+    # Smoothing line
+    checkboxInput('smooth', 'Smooth', T),
+    
+    # Set facets to display
+    selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
+    selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)), "am")
+  ),
+  
+  mainPanel(
+    plotOutput('plot')
+  )
+))
